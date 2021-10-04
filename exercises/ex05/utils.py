f309a5d747e56ab0a5587d458bb081e87d7c371e
Alayna <allor@email.unc.edu>
2021-10-03 23:52:50 -0400

    change

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,45 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730411361"
 
 # Define your functions below
+
+
+def only_evens(input: list[int]) -> list[int]:
+    """Return list of even ints in input."""
+    evens: list[int] = []
+    i: int = 0
+    while i < len(input):
+        if input[i] % 2 == 0:
+            evens.append(input[i])
+        i += 1
+    return evens
+    
+
+def sub(input: list[int], start: int, end: int) -> list[int]:
+    """Return list of input between start and end int."""
+    subset: list[int] = []
+    if len(input) == 0 or start > len(input) or end <= 0:
+        return subset
+    if start < 0:
+        start = 0
+    if end > len(input):
+        end = len(input)
+    while start < end:
+        subset.append(input[start])
+        start = start + 1
+    return subset
+
+
+def concat(a: list[int], b: list[int]):
+    """Return list that includes a and b."""
+    i: int = 0
+    new: list[int] = []
+    while i < len(a):
+        new.append(a[i])
+        i += 1
+    i = 0
+    while i < len(b):
+        new.append(b[i])
+        i += 1
+    return new
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,53 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+__author__ = "730411361"
+
+
+def test_only_evens_one() -> None:
+    """Testing regular list."""
+    assert only_evens([1, 2, 3, 4]) == [2, 4]
+
+
+def test_only_evens_two() -> None:
+    """Testing list with no evens."""
+    assert only_evens([1, 3, 5, 7, 9]) == []
+
+
+def test_only_evens_three() -> None:
+    """Testing empty list."""
+    assert only_evens([]) == []
+
+
+def test_sub_one() -> None:
+    """Testing empty list."""
+    assert sub([], 1, 1) == []
+
+
+def test_sub_two() -> None:
+    """Testing with negative start int."""
+    assert sub([1, 1, 1, 1], -5, 1) == [1]
+
+
+def test_sub_three() -> None:
+    """Testing regular ascending list."""
+    assert sub([1, 2, 3, 4], 0, 2) == [1, 2]
+
+
+def test_concat_one() -> None:
+    """Testing regular ascending lists."""
+    assert concat([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_two() -> None:
+    """Testing empty lists."""
+    # passed
+    assert concat([], []) == []
+
+
+def test_concat_three() -> None:
+    """Testing lists of different lengths."""
+    assert concat([3], [2, 1]) == [3, 2, 1]
