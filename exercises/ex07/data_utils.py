f31c8b2fe5dcbd33bf15baaa8513a4510859c54d
Alayna <allor@email.unc.edu>
2021-10-29 13:58:04 -0400

    changes

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -3,3 +3,36 @@
 __author__ = "123456789"
 
 # Define your functions below
+
+"""Some helpful utility functions for working with CSV files"""
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a table."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Product of list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table into a columb-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
